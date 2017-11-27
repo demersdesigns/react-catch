31421e34dc1df713ca914c3c1877efea206e2cfe
demersdesigns <paul@pdemersjr.com>
2017-11-27 18:48:33 -0500

    Start of store form input routing.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { render } from 'react-dom'
+import { render } from 'react-dom';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+  
+  goToStore(e) {
+    e.preventDefault();
+    //grab text from text input
+    console.log(this.storeInput.value);
+    //transition from / to /store/storeId
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name"/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
         <button type="submit">Visit Store</button>
       </form>
     )
